633625bdaf7293db3277411230831d06533766ca
Eduardo dos Anjos Souza <68042033+eduardoajs2020@users.noreply.github.com>
2022-09-23 23:44:51 -0300

    Java_intelij

---
+++ ExercicioAulaWiden3/Atividade3.java
@@ -0,0 +1,19 @@
+public class Atividade3 {
+    private int ano;
+    private String modelo;
+    private String marca;
+    private float velocidade;
+    private int numeroDaMarcha;
+
+    public Atividade3(int ano, String modelo, String marca, float velocidade, int numeroDaMarcha) {
+        this.ano = ano;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.velocidade = velocidade;
+        this.numeroDaMarcha = numeroDaMarcha;
+    }
+    public void acelerar(){
+
+
+    }
+}
---
+++ ExercicioAulaWiden3/TestaBike.java
@@ -0,0 +1,6 @@
+public class TestaBike {
+    public static void main(String[] args) {
+
+    Atividade3 = new Atividade3(,null, null, velocidade, numeroDaMarcha);
+    }
+}
